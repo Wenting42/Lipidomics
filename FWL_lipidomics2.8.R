83a4661064b87875d34ee50018c90a6893ca3c30
Wenting <34403625+Wenting42@users.noreply.github.com>
2019-07-18 11:49:52 -0400

    Update FWL_lipidomics2.8.R

--- FWL_lipidomics2.8.R
+++ FWL_lipidomics2.8.R
@@ -4,10 +4,10 @@
 # Notes:  To start, typing command in the console-----> source("FWL_lipidomics2.8.R") 
 #         or press the source butthon. 
 #         Please open the cytoscape on your computer and make sure Mac users installed XQuartz.
-#         This script is designed as a reference for lipidomics experiment. It is based
-#         on Niklas and Kenny's previous work (Their work files can be found in folders 
+#         This script is designed as a reference for lipidomics experiment. 
+#         It is based on Niklas and Kenny's previous work (Their work files can be found in folders 
 #         quality_control and statistics_quantification). Acknowledge to Weng's technical 
-#         guidance and Sebstian's shadow experiment.
+#         guidance, Laura's fatty acid saturation analysis project and Sebstian's shadow experiment
 #
 # Usage:  1) Type the name of transformed data file you want for analysis and choose it.
 #         2) Input the sample name of internal standandards seperated by space, 
